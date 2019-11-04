b6141d97578ba0eef67860894623b6590803b06d
Resay2k <yaser@ualberta.ca>
2019-11-03 23:36:25 -0700

    python script called on output

--- app.js
+++ app.js
@@ -14,9 +14,17 @@ app.post('/upload', upload.single('photo'), (req, res, next) => {
   // upload.single(photo) -> req.file is a single 'photo' file
     if(req.file) {
         console.log(req.file)
-        next()
-    }
-    else throw 'error';
+
+        var spawn = require("child_process").spawn;
+        var process = spawn('python',["./hello.py"]);
+
+
+        // Takes stdout data from script which executed
+        // with arguments and send this data to res object
+        process.stdout.on('data', function(data) {
+                res.send(data.toString());
+        })
+      } else throw 'error';
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
