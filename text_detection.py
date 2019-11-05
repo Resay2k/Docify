80cf050e1ca1f38f9701e7771440091abebcbfcd
Resay2k <yaser@ualberta.ca>
2019-11-04 23:51:50 -0700

    started implementing EAST detector

---
+++ text_detection.py
@@ -0,0 +1,94 @@
+# import the necessary packages
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import argparse
+import time
+import cv2
+
+# construct the argument parser and parse the cli arguments for:
+# --image : The path to our input image.
+# --east : The EAST scene text detector model file path.
+# --min-confidence : Probability threshold to determine text. Optional with default=0.5
+# --width : Resized image width — must be multiple of 32. Optional with default=320
+# --height : Resized image height — must be multiple of 32. Optional with default=320
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type=str,
+	help="path to input image")
+ap.add_argument("-east", "--east", type=str,
+	help="path to input EAST text detector")
+ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+	help="minimum probability required to inspect a region")
+ap.add_argument("-w", "--width", type=int, default=320,
+	help="resized image width (should be multiple of 32)")
+ap.add_argument("-e", "--height", type=int, default=320,
+	help="resized image height (should be multiple of 32)")
+args = vars(ap.parse_args())
+
+# load the input image and grab the image dimensions
+image = cv2.imread(args["image"])
+orig = image.copy()
+(H, W) = image.shape[:2]
+
+# set the new width and height and then determine the ratio in change
+# for both the width and height
+(newW, newH) = (args["width"], args["height"])
+rW = W / float(newW)
+rH = H / float(newH)
+
+# resize the image and grab the new image dimensions
+image = cv2.resize(image, (newW, newH))
+(H, W) = image.shape[:2]
+
+# resize the image and grab the new image dimensions
+image = cv2.resize(image, (newW, newH))
+(H, W) = image.shape[:2]
+
+# In order to perform text detection using OpenCV and the EAST deep learning model,
+# we need to extract the output feature maps of two layers:
+# define the two output layer names for the EAST detector model that
+# we are interested -- the first is the output probabilities - does a region contain
+# text or not? - and the second can be used to derive the bounding box coordinates of text
+layerNames = [
+	"feature_fusion/Conv_7/Sigmoid",
+	"feature_fusion/concat_3"]
+
+# load the pre-trained EAST text detector
+print("[INFO] loading EAST text detector...")
+net = cv2.dnn.readNet(args["east"])
+
+# construct a blob from the image and then perform a forward pass of
+# the model to obtain the two output layer sets
+blob = cv2.dnn.blobFromImage(image, 1.0, (W, H),
+	(123.68, 116.78, 103.94), swapRB=True, crop=False)
+
+# start counting time it takes to process image blob
+start = time.time()
+net.setInput(blob)
+
+# perform a forward pass of the model to obtain the two output layer sets:
+# 1. a scores map, containing the probability of a given region containing text
+# 2. a map used to derive the bounding box coordinates of text in our input images
+(scores, geometry) = net.forward(layerNames)
+end = time.time()
+
+# show timing information on text prediction
+print("[INFO] text detection took {:.6f} seconds".format(end - start))
+
+# grab the number of rows and columns from the scores volume, then
+# initialize our set of bounding box rectangles and corresponding
+# confidence scores
+(numRows, numCols) = scores.shape[2:4]
+rects = []
+confidences = []
+
+# loop over the number of rows
+for y in range(0, numRows):
+	# extract the scores (probabilities), followed by the geometrical
+	# data used to derive potential bounding box coordinates that
+	# surround text
+	scoresData = scores[0, 0, y]
+	xData0 = geometry[0, 0, y]
+	xData1 = geometry[0, 1, y]
+	xData2 = geometry[0, 2, y]
+	xData3 = geometry[0, 3, y]
+	anglesData = geometry[0, 4, y]
